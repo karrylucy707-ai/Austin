adfec597e6b282a9ae1071ee50e439713e361970
3y <zhongfucheng_123@163.com>
2025-11-12 20:44:54 +0800

    更新预览地址ip

--- stream-start.sh
+++ stream-start.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-docker cp ./austin-stream/target/austin-stream-0.0.1-SNAPSHOT.jar austin_jobmanager_1:/opt/
-docker exec -ti austin_jobmanager_1 flink run /opt/austin-stream-0.0.1-SNAPSHOT.jar
+BASE_DIR="$(cd "$(dirname "$0")" && pwd)"
+JAR_PATH="$BASE_DIR/austin-stream/target/austin-stream-0.0.1-SNAPSHOT.jar"
+if [[ ! -f "$JAR_PATH" ]]; then
+    echo "jar 不存在: $JAR_PATH" >&2
+        exit 1
+fi
+
+docker cp "$JAR_PATH" austin-jobmanager-1:/opt/austin-stream-0.0.1-SNAPSHOT.jar
+docker exec -ti austin-jobmanager-1 flink run /opt/austin-stream-0.0.1-SNAPSHOT.jar
 
 # stream local test
 # docker cp ./austin-stream-0.0.1-SNAPSHOT.jar austin_jobmanager_1:/opt/austin-stream-test-0.0.1-SNAPSHOT.jar
